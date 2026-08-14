291ea884e453b12fce7e3dfcb58b6d623aa08219
VIOLA895 <kambuniviola@gmail.com>
2026-08-14 14:33:33 +0300

    Temporarily remove Vercel analytics integrations

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/react"
-
 import App from "./App";
-
 import "./index.css";
 
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-  <Analytics>
-    <SpeedInsights>  
-      <React.StrictMode>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </React.StrictMode>
-    </SpeedInsights>
-  </Analytics>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
